8839dcdf1a6827bdb039181028f543d7092d7898
HabibaElalkamy00 <125804087+HabibaElalkamy00@users.noreply.github.com>
2023-09-12 03:12:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,245 @@
+/**function goto(){
+
+    if(document.getElementById("example").value = 'Ordinary Cinema'){
+        window.scrollTo(0,902)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Cars Cinema'){
+        window.scrollTo(0,902)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = '3D'){
+        window.scrollTo(0,1325)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = '9D'){
+        window.scrollTo(0,1456)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Bed Cinema'){
+        window.scrollTo(0,2008)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Private Cinema'){
+        window.scrollTo(0,2008)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Parking'){
+        window.scrollTo(0,2606)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Food'){
+        window.scrollTo(0,2606)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Kids Area'){
+        window.scrollTo(0,3311)
+        document.getElementById("example").value = ' '
+    }
+    else if(document.getElementById("example").value = 'Service Car'){
+        window.scrollTo(0,3311)
+        document.getElementById("example").value = ' '
+    }
+
+    
+}**/
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+function chair(){
+    document.getElementById('chair').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair2(){
+    document.getElementById('chair2').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair3(){
+    document.getElementById('chair3').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair4(){
+    document.getElementById('chair4').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair5(){
+    document.getElementById('chair5').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair6(){
+    document.getElementById('chair6').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair7(){
+    document.getElementById('chair7').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair8(){
+    document.getElementById('chair8').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair9(){
+    document.getElementById('chair9').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair10(){
+    document.getElementById('chair10').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair11(){
+    document.getElementById('chair11').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair12(){
+    document.getElementById('chair12').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair13(){
+    document.getElementById('chair13').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair14(){
+    document.getElementById('chair14').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair15(){
+    document.getElementById('chair15').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair16(){
+    document.getElementById('chair16').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair17(){
+    document.getElementById('chair17').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair18(){
+    document.getElementById('chair18').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair19(){
+    document.getElementById('chair19').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair20(){
+    document.getElementById('chair20').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair21(){
+    document.getElementById('chair21').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair22(){
+    document.getElementById('chair22').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair23(){
+    document.getElementById('chair23').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair24(){
+    document.getElementById('chair24').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair25(){
+    document.getElementById('chair25').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair26(){
+    document.getElementById('chair26').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair27(){
+    document.getElementById('chair27').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair28(){
+    document.getElementById('chair28').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair29(){
+    document.getElementById('chair29').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+function chair30(){
+    document.getElementById('chair30').src = 'icon/WhatsApp Image 2022-12-20 at 3.02.48 AM.jpeg';
+}
+
+
+
+function getOption() {         
+    selectElement = document.querySelector('#exampleInputtofcinema');         
+    output = selectElement.value; 
+    if(output ==  "Ordinary Cinema"){
+        document.querySelector('.output').value = '150 EGP';
+    }else if (output ==  "Cars Cinema") {
+        document.querySelector('.output').value = '400 EGP';
+    }else if(output ==  "3D"){
+        document.querySelector('.output').value = '350 EGP';
+    }else if(output ==  "9D"){
+        document.querySelector('.output').value = '500 EGP';
+    }else if(output ==  "Bed Cinema"){
+        document.querySelector('.output').value = '2000 EGP';
+    }else if(output ==  "Private Cinema"){
+        document.querySelector('.output').value = '5000 EGP';
+    }
+}  
+
+
+function getOption1() {         
+    selectElement = document.querySelector('#exampleInputpriceoffood');         
+    output1 = selectElement.value; 
+    if(output1 ==  "Large Popcorn"){
+        document.querySelector('.output1').value = '120 EGP';
+    }else if (output1 ==  "Medium Popcorn") {
+        document.querySelector('.output1').value = '100 EGP';
+    }else if(output1 ==  "Small Popcorn"){
+        document.querySelector('.output1').value = '60 EGP';
+    }
+}
+
+function getOption2() {         
+    selectElement = document.querySelector('#exampleInputpriceofdrink');         
+    output2 = selectElement.value; 
+    if(output2 ==  "Large Pepsi"){
+        document.querySelector('.output2').value = '50 EGP';
+    }else if (output2 ==  "Medium Pepsi") {
+        document.querySelector('.output2').value = '25 EGP';
+    }else if(output2 ==  "Small Pepsi"){
+        document.querySelector('.output2').value = '15 EGP';
+    }
+}
+
+
+
+
+
